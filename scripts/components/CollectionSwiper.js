17cf2508387255ccdf5757225e946bbdcc342391
kingbloopy <kingbloopy@gmail.com>
2023-01-14 13:01:45 -0700

    added tester carousel

--- scripts/components/CollectionSwiper.js
+++ scripts/components/CollectionSwiper.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const CollectionSwiper = ({ props }) => {
   const { collection_name, collection_description, images, linkInfo } = props;
+  const data = ['1', '2', '3', '4'];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const carouselInfiniteScroll = () => {
+    if (currentIndex === data.length-1){
+      return setCurrentIndex(0);
+    }
+    return setCurrentIndex(currentIndex+1);
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {carouselInfiniteScroll()}, 3000);
+    return () => clearInterval(interval);
+  });
 
   console.log(props);
 
@@ -15,9 +28,22 @@ const CollectionSwiper = ({ props }) => {
         </div>
       </div>
 
-      {images.map((image, i) => (
-        <img key={i} src={image.src}/>
-      ))}
+      <div className="carousel-container">
+        {images.map((image, i) => (
+          <img 
+            className="carousel-item"
+            style={{ transform: `translate(-${currentIndex * 100}%)` }} 
+            key={i} 
+            src={image.src}/>
+        ))}
+        {/* { data.map((item, index) => (
+          <h1
+          className="carousel-item"
+          style={{transform: `translate(-${currentIndex * 100}%)`}}
+          key={index}
+          >{item}</h1>
+        ))} */}
+      </div>
     </div>
   );
 }
